f4373f399ec05ce232fc71d917a6a6222237df31
danielfdzperez <daniel.fdz.perez@gmail.com>
2016-11-22 18:44:50 +0100

    point

--- javascript/point.js
+++ javascript/point.js
@@ -1,36 +1,28 @@
-/**
- * Clase Point
- *
- * Guarda las coordenas de una ubicacion
- */
 function Point(x, y){
 	this.x = x
 	this.y = y
 }
 
-/**
- * Compara dos puntos
- *
- * position -> Es el punto con el que se quiere comparar
- *
- * Return
- * Bool
- */
 Point.prototype.equals = function(position){
    return ((this.x == position.x) && (this.y == position.y))
 }
 
-/**
- * Genera un punto con posiciones aleatorias en un rango
- *
- * x_limit -> Limite del numero aleatorio en la posicion x
- * y_limit -> Limite del numero aleatorio en la posicion y
- *
- * Return
- * void
- */
 Point.prototype.random = function(x_limit, y_limit){
     this.x = Math.random() * x_limit
     this.y = Math.random() * y_limit
 }
 
+Point.prototype.getX() = function(){
+	return this.x
+}
+Point.prototype.getY() = function(){
+	return this.y
+}
+Point.prototype.setPoint(x, y) = function(){
+	this.x = x
+	this.y = y
+}
+
+Point.prototype.distance = function(point){
+   return Math.sqrt( Math.pow(this.getX() - point.getX() , 2) +  Math.pow(this.getY() - point.getY() , 2) )
+}
